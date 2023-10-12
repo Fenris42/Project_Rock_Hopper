cfaac695bdecd12f404596ada54ab0c137e8c9a8
Matt Todd <mtodd447@gmail.com>
2023-10-12 15:46:15 -0700

    Added Laser animation to gun

--- Assets/Scripts/Player/Laser.cs
+++ Assets/Scripts/Player/Laser.cs
@@ -3,19 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Dig : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     private Tilemap tilemap;
+    private Animator animator;
 
     void Start()
     {// Start is called before the first frame update
 
         tilemap = GameObject.Find("Destructible Tiles").GetComponent<Tilemap>();
+        animator = transform.Find("Laser").GetComponent<Animator>();
     }
         
     void Update()
     {// Update is called once per frame
 
+        PlayerInput();
+        DebugDig();
+
+
+    }
+
+    private void PlayerInput()
+    {
+        if (Input.GetMouseButton(0))
+        {//left mouse click
+            Dig();
+        }
+        else
+        {
+            animator.SetBool("Dig", false);
+        }
+    }
+
+    private void Dig()
+    {
+        animator.SetBool("Dig", true);
+    }
+
+    private void DebugDig()
+    {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButtonDown(0))
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -9,22 +9,26 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] float flySpeed;
 
     private Animator animator;
-    private SpriteRenderer sprite;
+    private Rigidbody2D rigidbody;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = transform.Find("Sprite").GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        PlayerInput();
     }
 
-    private void Movement()
+    // Movement /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void PlayerInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {//jetpack movement
@@ -48,28 +52,34 @@ public class Player_Movement : MonoBehaviour
     private void MoveLeft()
     {
         animator.SetBool("Run", true);
-        sprite.flipX = true;
+        transform.localScale = new Vector3(-1, 1, 1);
         transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
     }
 
     private void MoveRight()
     {
         animator.SetBool("Run", true);
-        sprite.flipX = false;
+        transform.localScale = new Vector3(1, 1, 1);
         transform.position += (Vector3.right * moveSpeed) * Time.deltaTime;
     }
 
     private void Fly()
     {
+        //resets downward gravity velocity to prevent stacking gravity while in flight
+        rigidbody.velocity = Vector3.zero;
+
+        //fly
         animator.SetBool("Fly", true);
         transform.position += (Vector3.up * flySpeed) * Time.deltaTime;
     }
 
+    // Collision Detection /////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {//player is grounded
             animator.SetBool("Fly", false);
+            
         }
     }
 
