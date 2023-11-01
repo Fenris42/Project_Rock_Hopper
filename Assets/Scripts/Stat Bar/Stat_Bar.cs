b6a27d81626b4af41dbc697660bcbce4332daa90
Matt Todd <mtodd447@gmail.com>
2023-11-01 13:33:23 -0700

    Added HUD
    - Added meter bars for health, oxygen, power
    - Added basic resource tracker

---
+++ Assets/Scripts/Stat Bar/Stat_Bar.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Stat_Bar : MonoBehaviour
+{
+    //private variables
+    private Slider fillSlider;
+    private Slider changeSlider;
+    private int current;
+    private int max;
+
+
+
+    void Start()
+    {// Start is called before the first frame update
+        fillSlider = GameObject.Find(gameObject.name + "/Border/Fill Slider").GetComponent<Slider>();
+        changeSlider = GameObject.Find(gameObject.name + "/Border/Change Slider").GetComponent<Slider>();
+    }
+
+    void Update()
+    {// Update is called once per frame
+
+    }
+
+    public void Initialize(int amount)
+    {//initialize bars current value
+        current = amount;
+        max = amount;
+    }
+
+    public void Add(int amount)
+    {//increase stat bar and play animation
+
+        //add amount from bar
+        current += amount;
+
+        //keep values in range
+        Sanitize();
+
+        //apply updates to secondary bar
+        UpdateChangeBar();
+
+        //delay updating the main bar for 1 sec
+        Invoke("UpdateFillBar", 0.5f);
+
+    }
+
+    public void Remove(int amount)
+    {//reduce stat bar and play animation
+
+        //remove amount from bar
+        current -= amount;
+
+        //keep values in range
+        Sanitize();
+
+        //apply updates to main bar
+        UpdateFillBar();
+
+        //delay updating the secondary bar for 1 sec
+        Invoke("UpdateChangeBar", 0.5f);
+    }
+
+    private void Sanitize()
+    {//ensure values stay in range
+
+        if (current < 0)
+        {
+            current = 0;
+        }
+        if (current > max)
+        {
+            current = max;
+        }
+    }
+
+    private void UpdateFillBar()
+    {//set slider to percentage of current and max values
+
+        //get bar fill ratio
+        float percentage = (float)current / (float)max;
+
+        //scale health bar fill
+        fillSlider.value = percentage;
+
+    }
+
+    private void UpdateChangeBar()
+    {//set slider to percentage of current and max values
+
+        //get bar fill ratio
+        float percentage = (float)current / (float)max;
+
+        //scale health bar fill
+        changeSlider.value = percentage;
+
+    }
+}
